da4bcfce596c476e7a8d47fa4acb175ad1ba6682
Julián Campos <m02capej@uco.es>
2022-04-22 11:53:23 +0200

    Creando botones

---
+++ app/src/main/java/es/juliancampos/ilernavet/Activity1.kt
@@ -0,0 +1,11 @@
+package es.juliancampos.ilernavet
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Activity1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.Activity1)
+    }
+}
